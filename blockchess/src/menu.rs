23f79b9b907499426d0bf6548b726f0a3e7c72e6
theArctesian <stephen.d.okita@gmail.com>
2023-01-06 14:32:38 +0800

    added wasm sup

--- blockchess/src/menu.rs
+++ blockchess/src/menu.rs
@@ -1,7 +1,4 @@
-//! This example illustrates how to create a button that changes color and text based on its
-//! interaction state.
-
-use bevy::{prelude::*, winit::WinitSettings};
+se bevy::{prelude::*, winit::WinitSettings};
 
 fn main() {
     App::new()
